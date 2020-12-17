c213eea5b81c879389612524ce51c5ea35d7a69a
mark l chaves <mark@marklchaves.com>
2020-12-17 17:32:19 +0800

    Don't allow direct access

--- log-lover.php
+++ log-lover.php
@@ -10,6 +10,10 @@ License: 		GPLv2 or later
 License URI:	http://www.gnu.org/licenses/gpl-2.0.html
 */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
 function logit_lover($log)
 {
    if (is_array($log) || is_object($log)) {
